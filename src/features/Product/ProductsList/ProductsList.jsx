ad7a0b060ac21652e6b981156280f916042a4c57
Alibek Tauirbaev <featternotving@gmail.com>
2025-02-23 23:49:27 +0500

    corection search

--- src/features/Product/ProductsList/ProductsList.jsx
+++ src/features/Product/ProductsList/ProductsList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import axios from 'axios';
 import ProductsListItem from "../ProductsListItem/ProductsListItem";
 import styles from "./ProductsList.module.css"
@@ -17,9 +17,61 @@ export const ProductsList = () => {
     const LIMIT = 9;
     const [totalPages, setTotalPages] = useState();
 
+    const [searchParams, setSearchParams] = useSearchParams();
+    const search = searchParams.get("search") || ""
+
+    const [mobile, setMobile] = useState(0);
+
+const handleWindowSizeChange = () => {
+  const widthWindow = window.innerWidth;
+  if (widthWindow) {
+    setMobile(widthWindow)
+  } 
+  
+  
+  
+  
+  
+}
+console.log(mobile);
+
+if (mobile < 500) {
+    console.log("ismobile");
+    
+}
+
+
+
+
+
+// useState
+
+// useEffect(() => {
+
+// }, [])
+
+useEffect(() => {
+  window.addEventListener('resize', handleWindowSizeChange);
+  
+  return () => {
+    window.removeEventListener('resize', handleWindowSizeChange);
+    console.log("dsf");
+  }
+}, []);
+
+
+
+
     useEffect(() => {
         setPage(1);
     }, [category]);
+
+    useEffect(() => {
+        if (category) {
+            setSearchParams({});
+        }
+    }, [category]);
+
 
     useEffect(() => {
         setIsLoading(true);
@@ -29,14 +81,24 @@ export const ProductsList = () => {
                 setTotalPages(Math.ceil(totalProducts / LIMIT));
             })
 
-        const url = category
-            ? `https://dummyjson.com/products/category/${category}?limit=${LIMIT * page}&skip=${(page - 1) * LIMIT}`
-            : "https://dummyjson.com/products?limit=9&skip=0";
+
+        const url = search
+            ? `https://dummyjson.com/products/search?q=${search}`
+            : category
+                ? `https://dummyjson.com/products/category/${category}?limit=${LIMIT}&skip=${(page - 1) * LIMIT}`
+                : "https://dummyjson.com/products?limit=9&skip=0";
+
+        console.log(url);
+        
+
+
+        // const url = search
+        // ? `https://dummyjson.com/products/search?q=${search}` 
 
         axios.get(url)
             .then(response => setProducts(response.data.products))
             .finally(() => setIsLoading(false));
-    }, [category, page]);
+    }, [category, page, search]);
 
     if (isLoading) {
         return (
@@ -64,13 +126,13 @@ export const ProductsList = () => {
                     <ProductsListItem
                         key={item.id}
                         {...item}
-                        cardImage = {item.thumbnail}
+                        cardImage={item.thumbnail}
                     />
                 ))}
             </div>
 
             <div className={styles.wrapperPagination}>
-                {totalPages > 1 && (
+                {!search && totalPages > 1 && (
                     <Box sx={{ display: "flex", justifyContent: "center", marginTop: 3 }}>
                         <Pagination
                             count={totalPages}
