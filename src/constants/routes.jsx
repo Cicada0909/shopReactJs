10719ed65f14677362ffc113d0c0facf29b3c1e1
Alibek Tauirbaev <featternotving@gmail.com>
2025-04-03 17:23:26 +0500

    add hashrouter

--- src/constants/routes.jsx
+++ src/constants/routes.jsx
@@ -1,14 +1,14 @@
-import { createBrowserRouter } from "react-router-dom";
-import PageLayout from "../layouts/PageLayout/PageLayout";
-import Home from "../pages/Home/Home";
-import { Cart } from "../pages/Cart/Cart";
-import Product from "../pages/Product/Product";
-import { pageRoutes } from "./pageRoutes";
-import Favorites from "../pages/Favorites/Favorites";
+import { createHashRouter } from 'react-router-dom'
+import PageLayout from '../layouts/PageLayout/PageLayout'
+import Home from '../pages/Home/Home'
+import { Cart } from '../pages/Cart/Cart'
+import Product from '../pages/Product/Product'
+import { pageRoutes } from './pageRoutes'
+import Favorites from '../pages/Favorites/Favorites'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
-        path: "/",
+        path: '/',
         element: <PageLayout />,
         children: [
             {
@@ -21,19 +21,18 @@ const router = createBrowserRouter([
             },
             {
                 path: pageRoutes.cartRoutes.cart,
-                element: <Cart />
+                element: <Cart />,
             },
             {
                 path: pageRoutes.productRoutes.product,
-                element: <Product />
+                element: <Product />,
             },
             {
                 path: pageRoutes.favoritesRoutes.favorites,
-                element: <Favorites />
+                element: <Favorites />,
             },
+        ],
+    },
+])
 
-        ]
-    }
-]);
-
-export default router;
+export default router
